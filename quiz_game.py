736b2f0225556473a3132265a2488fbd9c2dbfe4
kerlic <kerli@naver.com>
2026-08-09 22:55:56 +0900

    refactor: game

--- quiz_game.py
+++ quiz_game.py
@@ -1,6 +1,14 @@
 from io_controller import io_controller,ExitSignal
+from storage_handler import Storage
+
+QUIZ_PLAY = 1
+ADD_QUIZ = 2
+QUIZ_LIST = 3
+CHECK_SCORE = 4
 
 class QuizGame:
+  quiz_list = []
+
   def __init__(self):
     return
 
@@ -9,24 +17,32 @@ class QuizGame:
     while True:
       try:
         self.print_menu(io)
-        menu_num = io.ask_int("input int test: ", 1, 5)
-        print(menu_num)
+        menu_num = io.ask_int("선택: ", QUIZ_PLAY, CHECK_SCORE)
+        if menu_num == QUIZ_LIST:
+          loader = Storage()
+          data, status = loader.load()
+          if status == Storage.STATUS_OK:
+            print(data)
       except ExitSignal:
-        print("EXIT")
+        io.warn("EXIT")
         break
       except :
-        print("ERROR")
+        io.warn("ERROR")
         break
       finally:
-        print("finally")
+        io.info("finally")
 
   def print_menu(self, io):
     io.title("            🎯 QUIZ SHOW 🎯")
-    io.text("1. 퀴즈 풀기")
-    io.text("2. 퀴즈 추가")
-    io.text("3. 퀴즈 목록")
-    io.text("4. 점수 확인")
+    io.text(f"{QUIZ_PLAY}. 퀴즈 풀기")
+    io.text(f"{ADD_QUIZ}. 퀴즈 추가")
+    io.text(f"{QUIZ_LIST}. 퀴즈 목록")
+    io.text(f"{CHECK_SCORE}. 점수 확인")
     io.divider()
+
+  def quiz_loader(self, quizData):
+    return 1
+
 
 gameInstance = QuizGame()
 gameInstance.run_game()
